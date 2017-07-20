35cc73cc6e55120565d0a6d10f6a2287aaa73f89
Altice, Byron <byron.altice@nscorp.com>
2017-07-20 09:28:04 -0400

    AsyncWeirdness

---
+++ AsyncWeirdness.js
@@ -0,0 +1,24 @@
+//This all may or may not be syntactically correct.
+//I can't remember where I got this from
+
+
+function wait(ms) {
+	  return new Promise(r => setTimeout(r, ms)).then(() => "Yay");
+}
+
+// do some work in background.
+setInterval(() => 42, 200);
+
+async function test() {
+	debugger;
+	const hello = "world";
+	const response = await fetch('index.html');
+	const tmp = await wait(1000);
+	console.log(tmp);
+	return hello;
+}
+
+async function runTest() {
+	let result = await test();
+	console.log(result);
+}
